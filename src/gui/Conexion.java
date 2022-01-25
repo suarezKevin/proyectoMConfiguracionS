2b617288cae97a0daf7d6a3dadff5406fd8873a1
Kevins <suarezk453@gmail.com>
2022-01-24 22:35:33 -0500

    Realizar la conexion a la base de datos

---
+++ src/gui/Conexion.java
@@ -0,0 +1,38 @@
+package gui;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kevinssg12
+ */
+public class Conexion {
+    
+    Connection connect = null;
+    
+    public Connection conectar(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1/gestion_inmuebles","root","");
+            //JOptionPane.showMessageDialog(null, "Ok Conectado");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return connect;
+    }
+    
+}
