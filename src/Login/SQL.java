08005c65f0b5b39f4498195b7d4a9acad4d8657c
User <User@Dalembert>
2022-01-25 22:13:36 -0500

    Autoincrementos con SQL

---
+++ src/Login/SQL.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Login;
+
+import gui.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Dalembert
+ */
+public class SQL {
+
+    public static int id_inrementable() {
+        int id = 1;
+        try {
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            Conexion cn = new Conexion();
+            Connection cnx = cn.conectar();
+            ps = cnx.prepareStatement("select max(id_con) from contrato_pro_sec");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                id = rs.getInt(1) + 1;
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        } 
+        return id;
+    }
+    
+    public static int id_incrementableAlquilar() {
+        int id = 1;
+        try {
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            Conexion cn = new Conexion();
+            Connection cnx = cn.conectar();
+            ps = cnx.prepareStatement("select max(id_alq) from alquilar");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                id = rs.getInt(1) + 1;
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        } 
+        return id;
+    }
+
+}
