a2aec029e4f35c3ee6591b16eda33710e2dc46f8
Kevins <suarezk453@gmail.com>
2022-01-29 23:16:26 -0500

    Comentario para llamar al metodo de login

--- src/inmueblesmcs/InmueblesMCS.java
+++ src/inmueblesmcs/InmueblesMCS.java
@@ -17,6 +17,7 @@ public class InmueblesMCS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Llamar el login
         Login login = new Login();
         login.setVisible(true);
     }
